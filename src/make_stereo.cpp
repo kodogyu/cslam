dd6bcef8dfb9179c4306d3d7a0f6565bcbe925ca
kodogyu <kodogyu@khu.ac.kr>
2024-02-18 04:17:35 +0900

    Update make_stereo node

--- src/make_stereo.cpp
+++ src/make_stereo.cpp
@@ -1,14 +1,18 @@
 #include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 #include <sensor_msgs/Image.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
 
+typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> my_policy;
 class MakeStereo {
   public:
     MakeStereo() 
@@ -19,6 +23,7 @@ class MakeStereo {
       nh_.getParam("flag_continue", flag_continue_);
       nh_.getParam("flag_step", flag_step_);
       // topics
+      nh_.getParam("right_topic", right_topic_);
       nh_.getParam("color_topic", color_topic_);
       nh_.getParam("color_info", color_info_);
       nh_.getParam("depth_topic", depth_topic_);
@@ -27,6 +32,12 @@ class MakeStereo {
       nh_.getParam("gyro_topic", gyro_topic_);
       nh_.getParam("imu_topic", imu_topic_);
       nh_.getParam("tf_topic", tf_topic_);
+      // frame id
+      nh_.getParam("right_frame_id", right_frame_id_);
+      // focal length
+      nh_.getParam("focal_length", focal_length_);
+      // baseline
+      nh_.getParam("baseline", baseline_);
       // bag file paths
       nh_.getParam("input_filename", input_filename_);
       nh_.getParam("output_filename", output_filename_);
@@ -42,6 +53,9 @@ class MakeStereo {
       topics_.push_back(gyro_topic_);
       topics_.push_back(imu_topic_);
       topics_.push_back(tf_topic_);
+
+      // Publisher
+      right_image_publisher_ = nh_.advertise<sensor_msgs::Image>("/camera/color_right/image_raw", 5);
     }
 
     ~MakeStereo() {
@@ -50,7 +64,35 @@ class MakeStereo {
       output_bag_.close();
     }
 
-    void makeStereo() {
+    void getRightImage(cv::Mat const &left_image, cv::Mat const &depth_image, cv::Mat &right_image) {
+      // ROS_INFO_STREAM("calculating right image ...");
+      // ROS_INFO_STREAM("left image type : " << left_image.type());
+
+      short depth = 0;
+      int disparity = 0;
+      right_image = cv::Mat::zeros(left_image.rows, left_image.cols, left_image.type());
+
+      for (int row = 0; row < left_image.rows; row++) {
+          for (int col = 0; col < left_image.cols; col++) {
+              depth = depth_image.at<short>(row, col);
+              if (depth > 0) {
+                  disparity = (int)(focal_length_ * baseline_ / depth);
+                  // ROS_DEBUG_STREAM("(row, col): (" << row << ", " << col << ")");
+                  // ROS_DEBUG_STREAM("disparity: " << disparity);
+
+                  if (col - disparity > 0) {
+                      right_image.at<cv::Vec3b>(row, col - disparity) = left_image.at<cv::Vec3b>(row, col);
+                  }
+              }
+          }
+      }
+
+      ROS_INFO_STREAM("right image type : " << right_image.type());
+    }
+
+    // rosbag에서 color, depth 이미지를 추출해 
+    // right image를 만들어 output bag에 저장한다.
+    void makeStereo_bag() {
       bool got_color_image = false;
       bool got_depth_image = false;
       sensor_msgs::ImageConstPtr color_message_ptr;
@@ -94,10 +136,13 @@ class MakeStereo {
 
         // both frames are received
         if (got_color_image && got_depth_image) {
+          ROS_DEBUG_STREAM("color seq: " << color_image_ptr->header.seq
+                        << "depth seq: " << depth_image_ptr->header.seq);
           // calculate right image
           getRightImage(color_image_ptr->image, depth_image_ptr->image, right_image);
           right_image_ptr = color_image_ptr;
           right_image_ptr->image = right_image;
+          right_image_ptr->header.frame_id = "camera_color_right_optical_frame";
 
           // cv::imshow("left", color_image_ptr->image);
           // cv::imshow("right", right_image);
@@ -105,7 +150,7 @@ class MakeStereo {
 
           // Write to output bag file
           output_bag_.write("/camera/color/image_raw", m.getTime(), color_message_ptr);
-          output_bag_.write("/camera/depth/image_rect_raw", m.getTime(), depth_message_ptr);
+          output_bag_.write("/camera/aligned_depth_to_color/image_raw", m.getTime(), depth_message_ptr);
           output_bag_.write("/camera/color_right/image_raw", m.getTime(), right_image_ptr->toImageMsg());
           
           got_color_image = false;
@@ -115,34 +160,28 @@ class MakeStereo {
       
     }
 
-    void getRightImage(cv::Mat const &left_image, cv::Mat const &depth_image, cv::Mat &right_image) {
-      // ROS_INFO_STREAM("calculating right image ...");
-      // ROS_INFO_STREAM("left image type : " << left_image.type());
+    void callbackMakeStereo(const sensor_msgs::ImageConstPtr& left_message, const sensor_msgs::ImageConstPtr& depth_message) {
+      cv_bridge::CvImagePtr color_image_ptr = cv_bridge::toCvCopy(left_message, sensor_msgs::image_encodings::BGR8);
+      cv_bridge::CvImagePtr depth_image_ptr = cv_bridge::toCvCopy(depth_message, sensor_msgs::image_encodings::TYPE_16UC1);
+      cv::Mat right_image;
+      getRightImage(color_image_ptr->image, depth_image_ptr->image, right_image);
 
-      int focal_length = 604;  // f_x = 603.838, f_y = 604.019
-      int baseline = 10;  // 10mm
-      short depth = 0;
-      int disparity = 0;
-      right_image = cv::Mat::zeros(left_image.rows, left_image.cols, left_image.type());
-
-      for (int row = 0; row < left_image.rows; row++) {
-          for (int col = 0; col < left_image.cols; col++) {
-              depth = depth_image.at<short>(row, col);
-              if (depth > 0) {
-                  disparity = focal_length * baseline / depth;
-                  ROS_DEBUG_STREAM("(row, col): (" << row << ", " << col << ")");
-                  ROS_DEBUG_STREAM("disparity: " << disparity);
-
-                  if (col - disparity > 0) {
-                      right_image.at<cv::Vec3b>(row, col - disparity) = left_image.at<cv::Vec3b>(row, col);
-                  }
-              }
-          }
-      }
-
-      ROS_INFO_STREAM("right image type : " << right_image.type());
+      // make right image message
+      cv_bridge::CvImagePtr right_image_ptr = color_image_ptr;
+      right_image_ptr->image = right_image;
+      right_image_ptr->header.frame_id = "camera_color_right_optical_frame";
+      // Publish the topic
+      right_image_publisher_.publish(right_image_ptr->toImageMsg());
     }
 
+    // topic에서 color, depth 이미지를 subscribe하여
+    // right image를 publish한다.
+    void makeStereo_topic() {
+      message_filters::Subscriber<sensor_msgs::Image> color_image_subscriber(nh_, "/camera/color/image_raw", 1);
+      message_filters::Subscriber<sensor_msgs::Image> depth_image_subscriber(nh_, "/camera/depth/image_rect_raw", 1);
+      message_filters::Synchronizer<my_policy> color_depth_synchronizer(my_policy(10), color_image_subscriber, depth_image_subscriber);
+      color_depth_synchronizer.registerCallback(boost::bind(&MakeStereo::callbackMakeStereo, this, _1, _2));
+    }
   protected:
     // Flags
     bool flag_continue_;
@@ -153,6 +192,7 @@ class MakeStereo {
     std::string output_filename_;
 
     // Topics
+    std::string right_topic_;
     std::string color_topic_;
     std::string color_info_;
     std::string depth_topic_;
@@ -163,10 +203,20 @@ class MakeStereo {
     std::string tf_topic_;
     std::vector<std::string> topics_;
 
+    // Frame id
+    std::string right_frame_id_;
+
+    // Focal length
+    double focal_length_;
+    // baseline
+    double baseline_;
+
     // ROS variables
     ros::NodeHandle nh_;
     rosbag::Bag input_bag_;
     rosbag::Bag output_bag_;
+
+    ros::Publisher right_image_publisher_;
 };
 
 int main(int argc, char** argv) {
@@ -174,7 +224,7 @@ int main(int argc, char** argv) {
   MakeStereo make_stereo;
   ROS_INFO("node created");
   
-  make_stereo.makeStereo();
+  make_stereo.makeStereo_bag();
   
   ros::shutdown();
   return 0;
